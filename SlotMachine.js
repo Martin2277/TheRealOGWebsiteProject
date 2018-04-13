37c140f5ac361d7f548b01769e80fb67b36b8a4f
Jannik Mottulla <jannik228@szut.uni-bremen.de>
2018-04-13 11:21:21 +0200

    SlotMachine Datein erstellt

---
+++ SlotMachine.js
@@ -0,0 +1,15 @@
+var SlotMachineSymbols =["Stern", "Herz", "Kirsche", "Joker", "Herz", "Kirsche", "Reingeguckt"];
+var Guthaben = 0;
+var Result = [];
+function AddGuthaben(){
+  Guthaben += 10;
+document.getElementById("Guthaben").innerHTML = "Aktuelles Guthaben: " + Guthaben + "€";
+}
+function Spin(){
+  Result = [];
+	for(var i = 0; i < 3; i++){
+    var SlotNumber = Math.floor(Math.random() * SlotMachineSymbols.length)
+	Result.push(SlotMachineSymbols[SlotNumber])
+    document.getElementById("Slots").innerHTML = Result;
+}
+
