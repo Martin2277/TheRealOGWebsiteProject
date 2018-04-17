c4e721a53365059649324cbd4d9617484aef1a6a
Jannik Motulla <mottulla_j@newtrain2014.defence-elec.de>
2018-04-17 15:23:40 +0200

    NeueIdee

--- SlotMachine.js
+++ SlotMachine.js
@@ -1,15 +1,15 @@
-var SlotMachineSymbols =["Stern", "Herz", "Kirsche", "Joker", "Herz", "Kirsche", "Reingeguckt"];
+var SlotMachineSymbols = ["Stern", "Herz", "Kirsche", "Joker", "Herz", "Kirsche", "Reingeguckt"];
 var Guthaben = 0;
 var Result = [];
-function AddGuthaben(){
+function AddGuthaben() {
   Guthaben += 10;
-document.getElementById("Guthaben").innerHTML = "Aktuelles Guthaben: " + Guthaben + "€";
+  document.getElementById("Guthaben").innerHTML = "Aktuelles Guthaben: " + Guthaben + "ï¿½";
 }
-function Spin(){
+function Spin() {
   Result = [];
-	for(var i = 0; i < 3; i++){
+  for (var i = 0; i < 3; i++) {
     var SlotNumber = Math.floor(Math.random() * SlotMachineSymbols.length)
-	Result.push(SlotMachineSymbols[SlotNumber])
+    Result.push(SlotMachineSymbols[SlotNumber])
     document.getElementById("Slots").innerHTML = Result;
+  }
 }
-
